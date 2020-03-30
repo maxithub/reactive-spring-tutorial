ad754e94baa208671adfdf7c3a411c976dfb9d3c
Max Yuan <max.m.yuan@gmail.com>
2020-03-30 17:13:06 +0800

    intial commit

---
+++ src/main/java/max/lab/rst/ReactiveSpringTutorialApplication.java
@@ -0,0 +1,13 @@
+package max.lab.rst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSpringTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/max/lab/rst/ReactiveSpringTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package max.lab.rst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSpringTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
