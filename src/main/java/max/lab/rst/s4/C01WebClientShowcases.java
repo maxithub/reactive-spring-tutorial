f70e6331fd1f6b98b3fd9127ac7679731aa31116
Max Yuan <max.m.yuan@gmail.com>
2020-05-01 18:12:27 +0800

    Add WebClient showcase code

---
+++ src/main/java/max/lab/rst/s4/C01WebClientShowcases.java
@@ -0,0 +1,10 @@
+package max.lab.rst.s4;
+
+import org.springframework.web.reactive.function.client.WebClient;
+
+public class C01WebClientShowcases {
+    public static void main(String[] args) {
+//        var webclient = WebClient.create("http://localhost:8080/routed/");
+
+    }
+}
