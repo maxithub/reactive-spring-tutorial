b2e154562db7658c2f51db41a5f399ac1903c0e6
Max Yuan <max.m.yuan@gmail.com>
2020-05-30 18:36:16 +0800

    Added the dependency for spring cloud circuit breaker and resilience4j

---
+++ src/main/java/max/lab/rst/s08/C01ReactiveCircuitBreaker.java
@@ -0,0 +1,22 @@
+package max.lab.rst.s08;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class C01ReactiveCircuitBreaker {
+//    @Bean
+//    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+//        return factory -> {
+//            factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+//                    .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+//                    .timeLimiterConfig(TimeLimiterConfig.custom())
+//            );
+//        }
+//    }
+}
