6de3901799513fb8ad033ccce02ad31fe0cc1f2d
Max Yuan <max.m.yuan@gmail.com>
2020-07-06 18:38:40 +0800

    after video

--- src/main/java/max/lab/rst/s11/C01ThreadModel.java
+++ src/main/java/max/lab/rst/s11/C01ThreadModel.java
@@ -1,46 +1,51 @@
 package max.lab.rst.s11;
 
-import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
 import java.util.stream.Stream;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 public class C01ThreadModel {
 
-    public static void main(String[] args) throws InterruptedException {
-        Flux.fromStream(() -> {
-            System.out.printf("Producing the names in thread %s%n",
-                    Thread.currentThread().getName());
-                return Stream.of("jack", "joel", "joseph", "mac", "max", "micheal");
-            })
-//                .parallel()
-                .map(n -> {
-                    System.out.printf("Mapping#1 %s in thread %s%n", n,
-                            Thread.currentThread().getName());
-                    return n.toUpperCase();
-                })
-//                .publishOn(Schedulers.elastic())
-                .map(n -> {
-                    System.out.printf("Mapping#2 %s in thread %s%n", n,
-                            Thread.currentThread().getName());
-                    return n + "_X";
-                })
-//                .publishOn(Schedulers.elastic())
-//                .subscribeOn(Schedulers.elastic())
-                .doOnNext(n -> {
-                    System.out.printf("doOnNext %s in thread %s%n", n,
-                            Thread.currentThread().getName());
-                })
-//                .subscribeOn(Schedulers.elastic())
-                .parallel().runOn(Schedulers.elastic()).subscribe();
-        Thread.sleep(5000L);
-//                .subscribe();
-//                .runOn(Schedulers.parallel()).subscribe();
-
-//                .publishOn(Schedulers.elastic())
-//                .blockLast();
-
+    public static void main(String[] args) throws Exception {
+    	Flux.fromStream(() -> {
+    		System.out.printf("Producing stream in thread %s%n", 
+    				Thread.currentThread().getName());
+    		return Stream.of("max", "jack", "kate", "bill", "joe");
+    	})
+    	.parallel()
+    	.runOn(Schedulers.parallel())
+    	.map(n -> {
+    		System.out.printf("#1 Mapping %s in thread %s%n", n,
+    				Thread.currentThread().getName());
+    		return n.toUpperCase();
+    	})
+//    	.publishOn(Schedulers.elastic())
+    	.map(n -> {
+    		System.out.printf("#2 Mapping %s in thread %s%n", n,
+    				Thread.currentThread().getName());
+    		return n + "_X";
+    	})
+    	.doOnNext(n -> {
+    		System.out.printf("doOnNext %s in thread %s%n", n,
+    				Thread.currentThread().getName());
+    	}).subscribe();
+//    	.subscribeOn(Schedulers.elastic())
+//    	.subscribe();
+    	
+//    	Flux.fromStream(() -> {
+//    		System.out.printf("Producing stream in thread %s%n", 
+//    				Thread.currentThread().getName());
+//    		return Stream.of(1,2,3,4,5);
+//    	}).doOnNext(n -> {
+//    		System.out.printf("doOnNext %d in thread %s%n", n,
+//    				Thread.currentThread().getName());
+//    	})
+//    	.subscribe();
+    	
+    	TimeUnit.SECONDS.sleep(5L);
     }
 }
